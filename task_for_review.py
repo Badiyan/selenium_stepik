90c90fa20a9610624875323474356a023ce1d9ad
Badiyan <e.mailbox.sbv@gmail.com>
2020-09-03 19:00:34 +0300

    stepik task for review

---
+++ task_for_review.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+
+LINK = "http://suninjuly.github.io/registration1.html"
+
+# test values
+VALUE = 'Мой ответ'
+CLASS_VARS = ['first', 'second', 'third']
+bug = 0
+
+def fill_form(browser):
+    for var in CLASS_VARS:
+        try:
+            element = browser.find_element_by_css_selector('.first_block .form-group .{}'.format(var))
+            element.send_keys(VALUE)
+        except Exception as error:
+            print('Бага тут:\n',error)
+            global bug
+            bug = 1
+            exit(1)
+
+def click_submit(browser):
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+
+def run():
+    try:
+        browser = webdriver.Chrome()
+        browser.get(LINK)
+        fill_form(browser)
+        click_submit(browser)
+    finally:
+        if bug == 0:
+            report = 'без ошибок'
+        else:
+            report = 'с бажинкой'
+        print('Тест отработал {}\n'.format(report),'*'*100)
+        browser.quit()
+
+if __name__ == '__main__':
+    run()
